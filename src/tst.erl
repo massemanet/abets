69bf0ff1aaa481d44185e76364fce8771d845686
mats cronqvist <masse@klarna.com>
2011-03-11 15:56:58 +0100

    new btree logic w. some progress

--- src/tst.erl
+++ src/tst.erl
@@ -34,7 +34,7 @@
          eof=0,
          cache=[],
          blobs=[],
-         nodes=[[],[]]}).
+         nodes=[[]]}).
 
 go(L) ->
   finalize(lists:foldl(fun add_blob_f/2,#state{},lists:seq(1,L))).
@@ -46,7 +46,7 @@ finalize(S0 = #state{cache=[],blobs=Blobs}) ->
   case length(Blobs) =< S0#state.size of
     true ->
       Leaf = mk_leaf(Blobs,S0#state.eof),
-      S1 =
+      S2 =
         check_nodes(S0#state{nodes=add_leaf(Leaf,S0),
                              cache=Blobs++[Leaf],
                              eof=Leaf#node.pos+Leaf#node.size,
@@ -65,6 +65,42 @@ finalize(S0 = #state{cache=[],blobs=Blobs}) ->
                              cache=S1#state.cache++Blobs2++[Leaf2],
                              eof=Leaf2#node.pos+Leaf2#node.size,
                              blobs=[]})
+  end,
+  finalize_nodes(S2).
+
+finalize_nodes(S = #state{eof=Eof,cache=Cache,nodes=[Ns]}) ->
+  case length(Ns) =< S#state.size of
+    true ->
+      Root = mk_node(Ns,Eof),
+      NewEof = Root#node.pos+Root#node.size,
+      S#state{eof=NewEof,cache=Cache++[Root],nodes=[]};
+    false->
+      {N1s,N2s} = lists:split(length(Ns) div 2,Ns),
+      Node1 = mk_node(N1s,Eof),
+      NewEof1 = Node1#node.pos+Node1#node.size,
+      Node2 = mk_node(N2s,NewEof1),
+      NewEof2 = Node2#node.pos+Node2#node.size,
+      finalize_nodes(S#state{cache=Cache++[Node1,Node2],
+                             nodes=[Node1,Node2],
+                             eof=NewEof2})
+  end;
+finalize_nodes(S = #state{eof=Eof,cache=Cache,nodes=[NHs|NTs]}) ->
+  case length(NHs) =< S#state.size of
+    true ->
+      Node = mk_node(NHs,Eof),
+      NewEof = Node#node.pos+Node#node.size,
+      finalize_nodes(S#state{cache=Cache++[Node],
+                             nodes=add_node(Node,NTs),
+                             eof=NewEof});
+    false->
+      {NH1s,NH2s} = lists:split(length(NHs) div 2,NHs),
+      Node1 = mk_node(NH1s,Eof),
+      NewEof1 = Node1#node.pos+Node1#node.size,
+      Node2 = mk_node(NH2s,NewEof1),
+      NewEof2 = Node2#node.pos+Node2#node.size,
+      finalize_nodes(S#state{cache=Cache++[Node1,Node2],
+                             nodes=add_node(Node1,add_node(Node1,NTs)),
+                             eof=NewEof2})
   end.
 
 add_blob(Key,Val,S = #state{blobs=Blobs}) ->
@@ -99,7 +135,6 @@ chk_nodes(Nodes,#state{size=S,size2=S2,eof=Eof}) ->
   chk_nodes(Nodes,{[],[],Eof},{S,S2}).
 
 chk_nodes([],O,_)         -> O;
-chk_nodes([[]|_],O,_)     -> O;
 chk_nodes([N0s|Nss],{Ns,Cache,Eof},C={S,S2}) ->
   case S2 < length(N0s) of
     true ->
@@ -127,7 +162,7 @@ mk_blob(Key,Val) ->
         size=byte_size(Bin),
         type=Type}.
 
--record(tmp,{eof,bbs,len=0,min,max,zp=0}).
+-record(tmp,{eof,recs,len=0,min,max,zp}).
 
 mk_leaf(Blobs,Eof) ->
   T = lists:foldl(fun boff_f/2,#tmp{eof=Eof},Blobs),
@@ -137,32 +172,32 @@ mk_leaf(Blobs,Eof) ->
         min_key=T#tmp.min,
         max_key=T#tmp.max,
         pos=T#tmp.eof,
+        zero_pos=leaf,
         bin=Bin}.
 
 boff_f(#blob{key=Key,size=Size},T = #tmp{len=0,eof=Eof}) ->
-  T#tmp{eof=Eof+Size,bbs=[{Key,Eof}],len=1,min=Key,max=Key};
-boff_f(#blob{key=Key,size=Size},T = #tmp{eof=Eof,len=Len,bbs=BBs})->
-  T#tmp{eof=Eof+Size,bbs=BBs++[{Key,Eof}],len=Len+1,max=Key}.
+  T#tmp{eof=Eof+Size,recs=[{Key,Eof}],len=1,min=Key,max=Key};
+boff_f(#blob{key=Key,size=Size},T = #tmp{eof=Eof,len=Len,recs=Recs})->
+  T#tmp{eof=Eof+Size,recs=Recs++[{Key,Eof}],len=Len+1,max=Key}.
 
 leaf_disk_format(T) ->
-  term_to_binary({T#tmp.len,T#tmp.bbs}).
+  term_to_binary({T#tmp.len,T#tmp.recs}).
 
 mk_node(Nodes,Eof) ->
-  T = lists:foldl(fun noff_f/2,#tmp{eof=Eof},Nodes),
+  T = lists:foldl(fun noff_f/2,#tmp{},Nodes),
   Bin = int_disk_format(T),
   #node{length=T#tmp.len,
         size=byte_size(Bin),
         min_key=T#tmp.min,
         max_key=T#tmp.max,
-        pos=T#tmp.eof,
+        pos=Eof,
         zero_pos=T#tmp.zp,
         bin=Bin}.
 
-noff_f(#node{min_key=M,size=Size},T = #tmp{len=0,eof=Eof})->
-  T#tmp{eof=Eof+Size,zp=Eof,bbs=[],len=1,min=M,max=M};
-noff_f(#node{min_key=Min,max_key=Max,size=Size},T)->
-  #tmp{eof=Eof,len=Len,bbs=BBs} = T,
-  T#tmp{eof=Eof+Size,bbs=BBs++[{Min,Eof}],len=Len+1,max=Max}.
+noff_f(#node{pos=Pos,min_key=Min,max_key=Max},T = #tmp{len=0})->
+  T#tmp{zp=Pos,len=1,min=Min,max=Max,recs=[]};
+noff_f(#node{pos=Pos,max_key=Max,min_key=Min},T = #tmp{len=Len,recs=Recs})->
+  T#tmp{len=Len+1,max=Max,recs=Recs++[{Pos,Min}]}.
 
 int_disk_format(T) ->
-  term_to_binary({T#tmp.len,T#tmp.zp,T#tmp.min,T#tmp.max,T#tmp.bbs}).
+  term_to_binary({T#tmp.len,T#tmp.zp,T#tmp.min,T#tmp.max,T#tmp.recs}).
